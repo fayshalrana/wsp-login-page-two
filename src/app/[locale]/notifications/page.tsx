c68634ee78aecce4ed6f77f087cf2131f243a79d
fayshal <imfayshalrana@gmail.com>
2025-03-09 15:13:49 +0600

    fix error

--- src/app/[locale]/notifications/page.tsx
+++ src/app/[locale]/notifications/page.tsx
@@ -1,11 +1,11 @@
- "use client"
+"use client";
 
-import { NotificationsScreen } from "@/components/notifications/notifications"
+import { NotificationsScreen } from "@/components/notifications/notifications";
 
 interface NotificationsPageProps {
-  params: { locale: string }
+  params: { locale: string }; // Ensure `params` is an object, not a Promise
 }
 
 export default function Page({ params }: NotificationsPageProps) {
-  return <NotificationsScreen />
-} 
+  return <NotificationsScreen />;
+}
